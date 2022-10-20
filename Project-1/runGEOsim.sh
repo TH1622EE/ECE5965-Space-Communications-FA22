63e2bfce3e9356e0f571e9c607ecf930667763f2
TH1622EE <th1622EE@outlook.com>
2022-10-20 12:46:32 -0700

    Update runGEOsim.sh

--- Project-1/runGEOsim.sh
+++ Project-1/runGEOsim.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-# This script will launch MATLAB (local installation) and run the AEHF (GEO) sim
+# This script will launch MATLAB (local installation) and run the GEO sim
 
 cd MATLAB && matlab -r "run geoSim.m"
